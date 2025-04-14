8197632b6a2ff0c72d3b4999a0a94e77caaca854
holden <james.holden.moore@gmail.com>
2025-04-14 11:34:12 -0400

    submission edits

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import "./Footer.css";
 
+const currentYear = new Date().getFullYear();
+
 function Footer() {
   return (
     <footer className="footer">
       <p className="footer__name-surname">Developed by Name Surname</p>
-      <p className="footer__date">2022</p>
+      <p className="footer__date">{currentYear}</p>
     </footer>
   );
 }
