2d97a96b6671aa0b6d17d6aa19d4bd273f26c95f
holden <james.holden.moore@gmail.com>
2025-05-05 12:54:10 -0400

    current temperature unit

--- src/components/contexts/CurrentTempatureContext.jsx
+++
@@ -1,6 +0,0 @@
-// import React from "react";
-
-// // a context having the value of the current cnoice of temperature units
-// const CurrentTemperatureUnitContext = React.createContext();
-
-// export default CurrentTemperatureUnitContext;
