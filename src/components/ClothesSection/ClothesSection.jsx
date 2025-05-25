2705581a9b46a34ec12940c8a43cdd8f336b7ba3
holden <james.holden.moore@gmail.com>
2025-05-25 15:34:07 -0400

    styles

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,10 +1,11 @@
 import { defaultClothingItems } from "../../utils/constants";
 import ItemCard from "../ItemCard/ItemCard";
+import "./ClothesSection.css";
 
 function ClothesSection({ onCardClick }) {
   return (
     <div className="clothes-section">
-      <div>
+      <div className="clothes-section__content">
         <p className="clothes-section__title">Your items</p>
         <button className="clothes-section__button">+ Add New Item</button>
       </div>
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,4 +1,5 @@
 import avatar from "../../assets/avatar.svg";
+import "./SideBar.css";
 
 function SideBar() {
   return (
